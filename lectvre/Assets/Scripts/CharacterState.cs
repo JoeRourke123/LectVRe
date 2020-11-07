adb3c6ffa3d30f054c8cdabf6181bfc1f4b94278
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2020-11-07 13:27:12 +0000

    Added character state fields comment

--- lectvre/Assets/Scripts/CharacterState.cs
+++ lectvre/Assets/Scripts/CharacterState.cs
@@ -2,6 +2,6 @@ namespace DefaultNamespace
 {
 	public class CharacterState
 	{
-		
+		// userID, name, position [x, y, z and rotation], body colour, hair colour, face, pant colour, hair shape, hasHandUp,
 	}
 }
