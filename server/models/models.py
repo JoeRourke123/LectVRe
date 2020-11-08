b146412bcb4faa30360cdf3fa4e96cceeea2bcbb
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2020-11-08 04:18:21 +0000

    Begun making a menu and removed server side seat allocation

--- server/models/models.py
+++ server/models/models.py
@@ -10,7 +10,6 @@ class Lecturer(object):
         self.id: int = kwargs.get("id", 0)
         self.name: str = kwargs.get("name", "Jimmy Johnjohn")
         self.lecture: Lecture = kwargs.get("lecture", None)
-        self.position: tuple = kwargs.get("location", (0, 0, 0, 0))
         self.minifig: dict = kwargs.get("minifig", {
             "hair": None,
             "face": None,
@@ -22,7 +21,6 @@ class Lecturer(object):
 class Student(Lecturer):
     def __init__(self, **kwargs):
         Lecturer.__init__(self, **kwargs)
-        self.seat, self.position = kwargs.get("seat", (0,(0,0,0,0)))
         self.notes: list = []
         self.hand_up: bool = False
         self.depressed: bool = True
