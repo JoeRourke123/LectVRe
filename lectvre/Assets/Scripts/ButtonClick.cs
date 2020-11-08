f4452f9b46a265806fb7fb70b327e29557f05f88
seany067 <sean@escreet.co.uk>
2020-11-08 16:16:13 +0000

    Started work on buttons

---
+++ lectvre/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+
+    private static int TOTAL_TIME = 2;
+    private int TIME = 2;
+
+    private bool lookingAt = false;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() { 
+        if(lookingAt) {
+            TIME -= Time.timeDelta;
+            if(TIME < 0) {TIME = 0;}
+        }
+        if(lookingAt && TIME == 0) {
+            gameObject.GetComponent<Button>().onClick.invoke();
+        }
+    }
+
+    void OnPointerEnter() {
+        lookingAt = true;
+
+    }
+
+    void OnPointerExit() {
+        lookingAt = false;
+    }
+}
