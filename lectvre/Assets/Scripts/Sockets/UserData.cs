6f564a7381e05cd6c974e17a15a540e43019c482
seany067 <sean@escreet.co.uk>
2020-11-08 03:49:48 +0000

    Now removes people when they leave

--- lectvre/Assets/Scripts/Sockets/UserData.cs
+++ lectvre/Assets/Scripts/Sockets/UserData.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using System;
 
+/**
+* Used for holding user data in the game and used for receiving when a player leaves
+**/
 [Serializable]
-class UserData : MonoBehaviour{
+class UserData : MonoBehaviour, IMessageInterface{
     public string username;
-    public string id;
+    public string user;
+    public string type;
 
-    public UserData(string username, string id, GameObject gameObject) {
+    public UserData(string type, string username, string user, GameObject gameObject) {  
+        this.type = type;
         this.username = username;
-        this.id = id;
+        this.user = user;
+    }
+    public UserData(string username, string user, GameObject gameObject) {
+        this.username = username;
+        this.user = user;
+    }
+
+    public string toJson() {
+        return $"{{\"type\":\"{type}\",\"username\":\"{username}\", \"user\":\"{user}\"}}";
     }
 }
