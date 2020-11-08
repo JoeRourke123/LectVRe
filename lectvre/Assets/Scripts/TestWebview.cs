305b8f94ac2510c017dd85eebb799edc3e0f1b45
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2020-11-08 10:17:43 +0000

    Sort of getting it working soon maybe?

--- lectvre/Assets/Scripts/TestWebview.cs
+++ lectvre/Assets/Scripts/TestWebview.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 [RequireComponent(typeof (WebController))]
 public class TestWebview : MonoBehaviour {
 
     public Renderer render;
+    public string url;
+
+    private Texture2D tex;
+
+    private bool stopped = false;
+
     WebController webPlugin;
 
     // Start is called before the first frame update
     void Start() {
         webPlugin = GetComponent<WebController>();
+        Thread thread = new Thread(LoadWebsite);
+        thread.Start();
+    }
+
+    void Update()
+    {
+	    render.material.mainTexture = tex;
     }
 
     private void OnEnable() {
@@ -21,13 +35,32 @@ public class TestWebview : MonoBehaviour {
         WebController.resultRecieved -= GotResult;
     }
 
-    private void GotResult(byte[] bytes) {
-        Texture2D tex = new Texture2D(2, 2);
-        tex.LoadImage(bytes);
-        render.material.mainTexture = tex;
+    private Texture2D FlipTexture(Texture2D orig) {
+	    Texture2D flip = new Texture2D(orig.width,orig.height);
+		int xN = orig.width;
+	    int yN = orig.height;
+
+	    for(int i=0; i < xN; i++) {
+		    for(int j=0; j < yN; j++) {
+			    flip.SetPixel(xN-i-1, j, orig.GetPixel(i,j));
+		    }
+	    }
+
+	    flip.Apply();
+	    return flip;
     }
 
-    public void LoadWebsite(string url) {
-        webPlugin.GetImageFromURL(url);
+    private void GotResult(byte[] bytes) {
+        tex = new Texture2D(2, 2);
+        tex.LoadImage(bytes);
+        tex = FlipTexture(tex);
+    }
+
+    public void LoadWebsite()
+    {
+	    while (!stopped)
+	    {
+		    webPlugin.GetImageFromURL(url);
+	    }
     }
 }
